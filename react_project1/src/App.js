97ef029e05eeb3e3599df67a4300079551e10ff0
RJ_Raghu_tech <91701745+rjraghutech@users.noreply.github.com>
2022-10-09 11:47:36 +0530

    commiting

---
+++ react_project1/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Comp1 from './Components/Comp1';
+
+function App() {
+  return (
+      <Comp1/>
+  );
+}
+
+export default App;
---
+++ react_project1/src/Components/Comp1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Comp1() {
+
+    return (
+        <>
+            <form>
+                <textarea className='textArea' id='text'></textarea>
+                <button>Uppercase</button>
+            </form>
+        </>
+    )
+}
